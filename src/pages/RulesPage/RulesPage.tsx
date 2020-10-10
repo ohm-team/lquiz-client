4f43215d9a1b9b91a520f365de9e3772339f80af
Alexander Gusman <alex@goooseman.ru>
2020-10-10 13:04:42 +0300

    fix(rules page): style rules section

--- src/pages/RulesPage/RulesPage.tsx
+++ src/pages/RulesPage/RulesPage.tsx
@@ -5,6 +5,7 @@ import { T } from "react-targem";
 import { RootStackRoute, RootStackRoutes } from "../types";
 import styles from "./RulesPage.styles";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView } from "react-native";
 
 const RulesPage: React.FC<RulesPageProps> = ({
   navigation,
@@ -13,25 +14,31 @@ const RulesPage: React.FC<RulesPageProps> = ({
     navigation.navigate(routeName);
   };
   return (
-    <Card style={styles.container}>
-      <SafeAreaView>
-        <Button theme={DefaultTheme} onPress={handleLinkButtonClick("Home")}>
-          <T message="Home" />
-        </Button>
-        <Paragraph style={styles.header} accessibilityRole="header">
-          Rules
-        </Paragraph>
-        <Paragraph style={styles.text}>
-          <T message="Within one game, the user is assigned 15 questions." />
-        </Paragraph>
-        <Paragraph>
-          <T message="Points are awarded for each correct answer (quiz coins)" />
-        </Paragraph>
-        <Paragraph>
-          <T message="After passing all the questions, the general stratistics of the user is formed." />
-        </Paragraph>
-      </SafeAreaView>
-    </Card>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Card style={styles.card}>
+        <SafeAreaView>
+          <Button
+            theme={DefaultTheme}
+            mode="outlined"
+            onPress={handleLinkButtonClick("Home")}
+          >
+            <T message="Home" />
+          </Button>
+          <Paragraph style={styles.header} accessibilityRole="header">
+            Rules
+          </Paragraph>
+          <Paragraph style={styles.text}>
+            <T message="Within one game, the user is assigned 15 questions." />
+          </Paragraph>
+          <Paragraph>
+            <T message="Points are awarded for each correct answer (quiz coins)" />
+          </Paragraph>
+          <Paragraph>
+            <T message="After passing all the questions, the general stratistics of the user is formed." />
+          </Paragraph>
+        </SafeAreaView>
+      </Card>
+    </ScrollView>
   );
 };
 
