74d6d37195a4965fefafe9aba84a7f84df266f05
Alexander Gusman <alex@goooseman.ru>
2020-10-10 21:32:13 +0300

    fix(utils native): open link only once

--- src/utils/native.ts
+++ src/utils/native.ts
@@ -2,7 +2,7 @@ import { Linking, Platform } from "react-native";
 
 export const openLinkInNewTab = async (url: string) => {
   if (Platform.OS === "web") {
-    window.open(url, "_blank");
+    return window.open(url, "_blank");
   }
   await Linking.openURL(url);
 };
