f916581749611298e53132dc9be7ecfff61f7944
cakeinpanic <cakeinpanic@gmail.com>
2020-10-10 12:17:02 +0300

    update rules page

--- src/pages/RulesPage/RulesPage.styles.ts
+++ src/pages/RulesPage/RulesPage.styles.ts
@@ -4,6 +4,16 @@ export default StyleSheet.create({
   container: {
     minHeight: 200,
     minWidth: 200,
+    marginTop: 30,
     alignSelf: "center",
+    padding: 20,
+  },
+  header: {
+    fontSize: 30,
+    marginBottom: 20
+  },
+  text: {
+    fontSize: 15,
+    lineHeight: 20,
   },
 });
