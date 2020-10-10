16813852e9bf8fe1f64e7b8f83928ecb95951301
Alexander Gusman <alex@goooseman.ru>
2020-10-10 12:51:36 +0300

    fix(game page): add scroll view

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -12,6 +12,7 @@ import {
 } from "react-native-paper";
 import GamePageAnswer from "./GamePageAnswer";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView } from "react-native";
 
 const LeftContent: React.FC<CardTitleAddon> = (props: CardTitleAddon) => (
   <Avatar.Text {...props} label="Q" />
@@ -42,84 +43,86 @@ const GamePage: React.FC<GamePageProps> = ({
   );
 
   return (
-    <Card style={styles.card}>
-      <SafeAreaView>
-        <Card.Title
-          title={
-            <>
-              <T message={"Quiz question #"} />
-              {currentQuestionNumber}
-            </>
-          }
-          subtitle={
-            <>
-              <T message="out of" /> {totalQuestionsCount}
-            </>
-          }
-          left={LeftContent}
-          right={RightContent}
-        />
-        <Card.Cover
-          accessible={false}
-          source={{ uri: "https://source.unsplash.com/random?quiz" }}
-        />
-        {isQuestionLoading ? <ProgressBar indeterminate /> : null}
-        {question ? (
-          <Card.Content>
-            <Title style={styles.title}>
-              {question.count} {question.what.toLowerCase()} <T message="in" />{" "}
-              {question.where}.{" "}
-              <T message="What else do you think my contain number" />{" "}
-              {question.count}?
-            </Title>
-          </Card.Content>
-        ) : null}
-        {answers ? (
-          <Card.Actions style={styles.buttonsContainer}>
-            {answers.map((a) => (
-              <GamePageAnswer
-                key={a.id}
-                onAnswerClick={onAnswerClick}
-                style={styles.button}
-                contentStyle={styles.buttonContent}
-                isLoading={questionLoadingId === a.id}
-                isAnyQuestionLoading={questionLoadingId !== undefined}
-                isSelected={selectedAnswerId === a.id}
-                isCorrectAnswer={a.id === correctAnswerId}
-                isAnswerRevealed={isNextButtonVisible}
-                {...a}
-              />
-            ))}
-            {isNextButtonVisible ? (
-              <Button
-                mode="outlined"
-                style={styles.button}
-                contentStyle={styles.buttonContent}
-                onPress={onNextButtonClick}
-              >
-                <T message="Next question!" />
-              </Button>
-            ) : null}
-          </Card.Actions>
-        ) : null}
+    <ScrollView>
+      <Card style={styles.card}>
+        <SafeAreaView>
+          <Card.Title
+            title={
+              <>
+                <T message={"Quiz question #"} />
+                {currentQuestionNumber}
+              </>
+            }
+            subtitle={
+              <>
+                <T message="out of" /> {totalQuestionsCount}
+              </>
+            }
+            left={LeftContent}
+            right={RightContent}
+          />
+          <Card.Cover
+            accessible={false}
+            source={{ uri: "https://source.unsplash.com/random?quiz" }}
+          />
+          {isQuestionLoading ? <ProgressBar indeterminate /> : null}
+          {question ? (
+            <Card.Content>
+              <Title style={styles.title}>
+                {question.count} {question.what.toLowerCase()}{" "}
+                <T message="in" /> {question.where}.{" "}
+                <T message="What else do you think my contain number" />{" "}
+                {question.count}?
+              </Title>
+            </Card.Content>
+          ) : null}
+          {answers ? (
+            <Card.Actions style={styles.buttonsContainer}>
+              {answers.map((a) => (
+                <GamePageAnswer
+                  key={a.id}
+                  onAnswerClick={onAnswerClick}
+                  style={styles.button}
+                  contentStyle={styles.buttonContent}
+                  isLoading={questionLoadingId === a.id}
+                  isAnyQuestionLoading={questionLoadingId !== undefined}
+                  isSelected={selectedAnswerId === a.id}
+                  isCorrectAnswer={a.id === correctAnswerId}
+                  isAnswerRevealed={isNextButtonVisible}
+                  {...a}
+                />
+              ))}
+              {isNextButtonVisible ? (
+                <Button
+                  mode="outlined"
+                  style={styles.button}
+                  contentStyle={styles.buttonContent}
+                  onPress={onNextButtonClick}
+                >
+                  <T message="Next question!" />
+                </Button>
+              ) : null}
+            </Card.Actions>
+          ) : null}
 
-        {selectedAnswerId ? (
-          correctAnswerId === selectedAnswerId ? (
-            <>
-              {/* eslint-disable-next-line @typescript-eslint/no-empty-function */}
+          {selectedAnswerId ? (
+            correctAnswerId === selectedAnswerId ? (
+              <>
+                {/* eslint-disable-next-line @typescript-eslint/no-empty-function */}
+                <Snackbar visible onDismiss={() => {}}>
+                  <T message="Hooray! This is the correct answer!" />
+                </Snackbar>
+              </>
+            ) : (
+              // eslint-disable-next-line @typescript-eslint/no-empty-function
               <Snackbar visible onDismiss={() => {}}>
-                <T message="Hooray! This is the correct answer!" />
+                <T message="Ohh nooo... This is an incorrect answer..." />
               </Snackbar>
-            </>
-          ) : (
-            // eslint-disable-next-line @typescript-eslint/no-empty-function
-            <Snackbar visible onDismiss={() => {}}>
-              <T message="Ohh nooo... This is an incorrect answer..." />
-            </Snackbar>
-          )
-        ) : null}
-      </SafeAreaView>
-    </Card>
+            )
+          ) : null}
+        </SafeAreaView>
+      </Card>
+    </ScrollView>
   );
 };
 
