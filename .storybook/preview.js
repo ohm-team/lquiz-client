069ac9619a7688d75ce042564fe65c3affae306e
Alexander Gusman <alex@goooseman.ru>
2020-10-10 10:29:15 +0300

    chore(storybook): add theme switches to .storybook/preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,7 +3,13 @@ import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 import "@storybook/addon-console";
 import TargemProvider from "react-targem";
 import translations from "src/i18n/translations.json";
-import { Provider as PaperProvider } from "react-native-paper";
+import {
+  Provider as PaperProvider,
+  DefaultTheme,
+  DarkTheme,
+} from "react-native-paper";
+
+const IS_DARK_THEME = false;
 
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/controls#show-full-documentation-for-each-property
@@ -31,7 +37,7 @@ export const globalTypes = {
 
 export const decorators = [
   (Story, { globals }) => (
-    <PaperProvider>
+    <PaperProvider theme={IS_DARK_THEME ? DarkTheme : DefaultTheme}>
       <TargemProvider locale={globals.locale} translations={translations}>
         <Story />
       </TargemProvider>
